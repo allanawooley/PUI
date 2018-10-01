7bafb127e948b233a0d785517b0241df9beb0054
allanawooley <38119678+allanawooley@users.noreply.github.com>
2018-10-01 11:12:26 -0400

    Update main.js

--- week6_lab/main.js
+++ week6_lab/main.js
@@ -1,4 +1,6 @@
 
+<!--jquery-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!-- javascript -->
 <script src="main.js" type="text/javascript"></script>
 
